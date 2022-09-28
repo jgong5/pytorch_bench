bfa53b15d8b622103fe022a411baae550f587b7b
Jiong Gong <jiong.gong@intel.com>
2022-09-28 14:11:52 +0000

    add two test files

--- operator_benchmark/test_softmax_dynamic_4d_reuse/output_code_inplace_buf.py
+++ operator_benchmark/test_softmax_dynamic_4d_reuse/output_code_inplace_buf.py
@@ -11,65 +11,68 @@ aten = torch.ops.aten
 kernel0 = CppCodeCache.load('''
 #include "/tmp/torchinductor_jgong5/qr/cqrr7t6pdy7hpf76525uh7ddg65eljkjlgxa5dhk7amq6xb6a3ia.h"
 extern "C" void kernel(const float* __restrict__ in_ptr0,
-                       float* __restrict__ out_ptr0,
-                       float* __restrict__ out_ptr1,
+                       float* __restrict__ out_ptr2,
+                       float* __restrict__ out_ptr3,
                        const long ks0,
                        const long ks1)
 {
-    float* out_ptr2 = out_ptr0; float* out_ptr3 = out_ptr1;
-    #pragma GCC ivdep
-    for(long i0=0; i0<ks0*ks1; ++i0)
+    float* out_ptr0 = out_ptr2;
+    float* out_ptr1 = out_ptr3;
+    #pragma omp parallel num_threads(28)
     {
-        {
-            {
-                    using Vec = at::vec::Vectorized<float>;
-                    Vec tmp1(-std::numeric_limits<float>::infinity());
-                    for(long i1=0; i1<ks1/8; ++i1)
-                    {
-                        {
-                            Vec tmp0 = Vec::loadu(in_ptr0 + i1*8 + (i0*ks1));
-                            Vec tmp1 = at::vec::maximum(tmp1, tmp0);
-                        }
-                    }
-                    out_ptr0[i0] = at::vec::vec_reduce_all([](Vec& x, Vec& y) { return at::vec::maximum(x, y); }, tmp1);
-            }
-        }
-    //}
-    //#pragma GCC ivdep
-    //for(long i0=0; i0<ks0*ks1; ++i0)
-    //{
-        {
-            {
-                    using Vec = at::vec::Vectorized<float>;
-                    Vec tmp4(0);
-                    assert(ks1 % 8 == 0);
-                    for(long i1=0; i1<ks1 / 8; ++i1)
-                    {
-                        {
-                            Vec tmp0 = Vec::loadu(in_ptr0 + i1 * 8 + (i0*ks1));
-                            Vec tmp1 = Vec::loadu(out_ptr0 + i0);
-                            Vec tmp2 = tmp0 - tmp1;
-                            Vec tmp3 = tmp2.exp();
-                            tmp3.store(out_ptr1 + i1 * 8 + (i0*ks1));
-                            tmp4 += tmp3;
-                        }
-                    }
-                    out_ptr2[i0] = at::vec::vec_reduce_all([](Vec& x, Vec& y) { return x+y; }, tmp4);
-            }
-        }
-    //}
-    //#pragma GCC ivdep
-    //for(long i0=0; i0<ks0*ks1; ++i0)
-    //{
-        #pragma omp simd simdlen(8)
-        for(long i1=0; i1<ks1; ++i1)
+        #pragma omp for
+        for(long i0=0; i0<ks0*ks1; ++i0)
         {
             {
                 {
-                    auto tmp0 = out_ptr1[i1 + (i0*ks1)];
-                    auto tmp1 = out_ptr2[i0];
-                    auto tmp2 = tmp0 / tmp1;
-                    out_ptr3[i1 + (i0*ks1)] = tmp2;
+                    float tmp1 = -std::numeric_limits<float>::infinity();
+                    #pragma omp simd simdlen(8) reduction(max:tmp1)
+                    for(long i1=0; i1<ks1; ++i1)
+                    {
+                        {
+                            auto tmp0 = in_ptr0[i1 + (i0*ks1)];
+                            tmp1 = std::max(tmp1, tmp0);
+                        }
+                    }
+                    out_ptr0[i0] = tmp1;
+                }
+            }
+        }
+        #pragma omp for
+        for(long i0=0; i0<ks0*ks1; ++i0)
+        {
+            {
+                {
+                    float tmp4 = 0;
+                    #pragma omp simd simdlen(8) reduction(+:tmp4)
+                    for(long i1=0; i1<ks1; ++i1)
+                    {
+                        {
+                            auto tmp0 = in_ptr0[i1 + (i0*ks1)];
+                            auto tmp1 = out_ptr0[i0];
+                            auto tmp2 = tmp0 - tmp1;
+                            auto tmp3 = std::exp(tmp2);
+                            out_ptr1[i1 + (i0*ks1)] = tmp3;
+                            tmp4 += tmp3;
+                        }
+                    }
+                    out_ptr2[i0] = tmp4;
+                }
+            }
+        }
+        #pragma omp for
+        for(long i0=0; i0<ks0*ks1; ++i0)
+        {
+            #pragma omp simd simdlen(8)
+            for(long i1=0; i1<ks1; ++i1)
+            {
+                {
+                    {
+                        auto tmp0 = out_ptr1[i1 + (i0*ks1)];
+                        auto tmp1 = out_ptr2[i0];
+                        auto tmp2 = tmp0 / tmp1;
+                        out_ptr3[i1 + (i0*ks1)] = tmp2;
+                    }
                 }
             }
         }
